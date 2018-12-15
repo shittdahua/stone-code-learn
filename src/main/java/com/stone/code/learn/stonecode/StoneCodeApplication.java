ea748117ed41f4fde6b56eb4d7dd9bf928ef897e
TT <tt2stone@163.com>
2018-12-15 19:50:44 +0800

    init

---
+++ src/main/java/com/stone/code/learn/stonecode/StoneCodeApplication.java
@@ -0,0 +1,14 @@
+package com.stone.code.learn.stonecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoneCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoneCodeApplication.class, args);
+	}
+
+}
+
